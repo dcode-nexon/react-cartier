8d0836b753818a46ef53c1e5b58dc4a6e7bcd8b9
tubi55 <tubi55@nate.com>
2022-10-27 15:19:17 +0900

    styled-components 기본 사용법

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-i
+//npm i styled-components
+
+import styled from 'styled-components';
+
+const Wrapper = styled.article`
+	position: fixed;
+	width: 100%;
+	height: 100%;
+	min-width: 1024px;
+	min-height: 768px;
+	background: url(${(props) => props.bg}) no-repeat center/cover;
+`;
 
 function App() {
 	return (
 		<>
-
+			<Wrapper bg={process.env.PUBLIC_URL + '/img/bg.jpg'}></Wrapper>
 		</>
 	);
 }
